731b2f9e6974ba3463e7707051d21bc14e38a1b3
michael-leibolt <mike.leibolt1.1@gmail.com>
2025-08-29 12:59:40 -0400

    working knex through to pg

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 
 const express = require('express')
 const cors = require('cors')
+const knex = require('knex')( require('./knexfile')[process.env.NODE_ENV])
 const app = express()
 const port = process.env.EXPRESS_PORT
 
@@ -12,6 +13,22 @@ const port = process.env.EXPRESS_PORT
 app.listen(port, () => `Express has started on ${port}`)
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Movies API')
+})
+
+app.get('/movies', (req,res) => {
+  knex('favorites')
+  .select()
+  .from('favorites')
+  .then(data => {
+    res.status(200).send(data)
+  })
+  .catch(err => {
+    if(err){
+      res.status(404).send(`Database is unable to respond to that request \n ${err}`)
+    }
+  })
+
+
 })
 
--- backend/knexfile.js
+++ backend/knexfile.js
@@ -1,47 +1,18 @@
 // Update with your config settings.
-
+require('dotenv').config()
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
 module.exports = {
 
   development: {
-    client: 'sqlite3',
+    client: 'pg',
     connection: {
-      filename: './dev.sqlite3'
-    }
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
+      host: process.env.DB_HOST,
+      password: process.env.DB_PASSWORD,
+      user: process.env.DB_USER,
+      port: process.env.DB_PORT,
+      database: process.env.DB_NAME
     }
   }
-
 };
